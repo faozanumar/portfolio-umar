66f88cc6afc841467266c11ec6dd966a83b54e18
faozanumar <umarnur30@gmail.com>
2026-03-16 10:07:59 +0700

    update tools

--- src/data.js
+++ src/data.js
@@ -96,6 +96,12 @@ export const listTools = [
     ket: "Design App",
     dad: "1100",
   },
+  {
+    id: 12,
+    nama: "Tool Lainnya",
+    ket: "Masih banyak tools.",
+    dad: "1200",
+  },
 ];
 
 import Proyek1 from "/assets/proyek/proyek1.webp";
